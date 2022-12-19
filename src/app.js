62335cf95769e11eb63edb68e6b4ff11f9c40a8a
Victor-Mannelli <imp.mannelli@gmail.com>
2022-12-19 11:52:56 -0300

    feat: add connection to db and libs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express, { json } from "express";
 import cors from "cors";
-import connection from "./database/database";
+import connection from "./database/database.js";
 
 const app = express();
 app.use(cors());
